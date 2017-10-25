ceb06e02a3b9efedb78de3671977109460e68a53
amitiwary999 <tiwarya9@gmail.com>
2017-10-26 05:16:15 +0530

    added view for recorder

--- app/src/main/java/com/example/meeera/cutsong/Fragment/Recorder.kt
+++ app/src/main/java/com/example/meeera/cutsong/Fragment/Recorder.kt
@@ -1,9 +1,18 @@
 package com.example.meeera.cutsong.Fragment
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.meeera.cutsong.R
 
 /**
  * Created by meeera on 24/10/17.
  */
 class Recorder() : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = inflater?.inflate(R.layout.record_fragment, container, false)
+        return view
+    }
 }
